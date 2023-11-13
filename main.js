3220f58a309ad6991556ed5624c83f46f9384dc5
KhanhAn18 <pkhanhan1818@gmail.com>
2023-11-13 20:16:47 +0700

    baiTapChoNhom

---
+++ main.js
@@ -0,0 +1,7 @@
+var elements = document.querySelectorAll(".product");
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].innerHTML += "<p>gia cu :9999</p>";
+    }
+    
+var themclass = document.querySelectorAll(".product");
